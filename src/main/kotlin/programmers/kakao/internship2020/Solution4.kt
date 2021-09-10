799d130202b6f347d43b7456d689a5e5bf007c0b
JangMinSuk <jms8732@naver.com>
2021-09-10 14:19:16 +0900

    💡 kakao-internship 2020

--- src/main/kotlin/programmers/kakao/internship2020/Solution4.kt
+++ src/main/kotlin/programmers/kakao/internship2020/Solution4.kt
@@ -1,19 +1,30 @@
 package programmers.kakao.internship2020
 
 import java.util.*
+import kotlin.Comparator
 import kotlin.collections.ArrayList
+import kotlin.math.abs
 import kotlin.math.min
 
+/*
+경주로 건설
+풀이 방법: 다익스트라를 이용한 최소 비용 구하기
+
+ */
 fun main(args: Array<String>) {
     val sol = Solution4()
 
     println(
         sol.solution(
             arrayOf(
-                intArrayOf(0, 0, 1, 0),
-                intArrayOf(0, 0, 0, 0),
-                intArrayOf(0, 1, 0, 1),
-                intArrayOf(1, 0, 0, 0)
+                intArrayOf(0, 0, 0, 0, 0, 0, 0, 1),
+                intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+                intArrayOf(0, 0, 0, 0, 0, 1, 0, 0),
+                intArrayOf(0, 0, 0, 0, 1, 0, 0, 0),
+                intArrayOf(0, 0, 0, 1, 0, 0, 0, 1),
+                intArrayOf(0, 0, 1, 0, 0, 0, 1, 0),
+                intArrayOf(0, 1, 0, 0, 0, 1, 0, 0),
+                intArrayOf(1, 0, 0, 0, 0, 0, 0, 0)
             )
         )
     )
@@ -21,87 +32,77 @@ fun main(args: Array<String>) {
 
 class Solution4 {
     fun solution(board: Array<IntArray>): Int {
-        val visited = Array(2) {
-            Array(board.size) {
-                BooleanArray(board[it].size)
+        val pq = PriorityQueue<Node>(Comparator<Node> { o1, o2 ->
+            o1.cost.compareTo(o2.cost)
+        }).apply {
+            for (i in 0 until 4) {
+                add(Node(0, 0, 0, i))
             }
-        }.apply {
-            this[0][0][0] = true
-            this[1][0][0] = true
         }
 
-        val cost = arrayListOf<IntArray>().apply {
-            for (i in board.indices) {
-                add(IntArray(board[i].size) {
+        val cost = Array(4){
+            Array(board.size){
+                IntArray(board.size){
                     Int.MAX_VALUE
-                })
+                }
             }
-        }.apply {
-            this[0][0] = 0
         }
 
-        return bfs(board, visited, cost)
-    }
-
-    private fun bfs(board: Array<IntArray>, visited: Array<Array<BooleanArray>>, cost: ArrayList<IntArray>): Int {
-        val q = LinkedList<Point>().apply {
-            add(Point(0, 0, 0))
-            add(Point(0, 0, 1))
-        }
-
-        val ud = arrayOf(-1, 0, 1, 0)
+        val ud = arrayOf(1, 0, -1, 0)
         val rl = arrayOf(0, 1, 0, -1)
-
-        while (!q.isEmpty()) {
-            val cur = q.poll()
-
-            println(cost.contentPrintln())
-
-            visited[cur.dir][cur.x][cur.y] = true
+        while (!pq.isEmpty()) {
+            val cur = pq.poll()
 
             for (i in ud.indices) {
                 val nx = cur.x + ud[i]
                 val ny = cur.y + rl[i]
 
-                val ndir = i % 2
-
-                if (nx < 0 || nx >= board.size || ny < 0 || ny >= board[0].size
-                    || board[nx][ny] == 1
-                )
+                //뒤로 돌아가는 경우
+                if(abs(cur.dir - i) == 2)
                     continue
 
-                cost[nx][ny] = min(
-                    cost[nx][ny], cost[cur.x][cur.y] + when (ndir == cur.dir) {
-                        true -> 100
-                        else -> 600
-                    }
-                )
+                if (nx < 0 || nx >= board.size || ny < 0 || ny >= board[0].size || board[nx][ny] == 1)
+                    continue
 
-                if (!visited[ndir][nx][ny]) {
-                    visited[ndir][nx][ny] = true
-                    q.add(Point(nx, ny, ndir))
+                if(cost[i][nx][ny] > cur.cost + cost(cur.dir,i)) {
+                    cost[i][nx][ny] = cur.cost + cost(cur.dir,i)
+                    pq.add(
+                        Node(
+                            x = nx,
+                            y = ny,
+                            cost = cur.cost + cost(cur.dir, i),
+                            dir = i
+                        )
+                    )
                 }
             }
         }
 
-        cost.contentPrintln()
-
-        return cost[board.lastIndex][board[0].lastIndex]
-    }
-
-    private fun ArrayList<IntArray>.contentPrintln(){
-        this.forEach {
-            it.forEach{
-                print("$it ")
-            }
-            println()
+        var min = Int.MAX_VALUE
+        for(i in cost.indices){
+            min = min(min,cost[i][board.lastIndex][board.lastIndex])
         }
-        println()
+
+        return min
     }
 
-    data class Point(
+    private fun cost(preDir : Int , curDir : Int) : Int{
+        return when(preDir){
+            0, 2-> {
+                if(curDir == 1 || curDir == 3) 600
+                else  100
+            }
+            else -> {
+                if(curDir == 0 || curDir == 2) 600
+                else 100
+            }
+        }
+    }
+
+    data class Node(
         val x: Int,
         val y: Int,
+        val cost: Int,
         val dir: Int
     )
 }
