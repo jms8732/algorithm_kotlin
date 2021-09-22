37a1ab0d7147c0f22e8517e0051a3eb89a404b3f
JangMinSuk <jms8732@naver.com>
2021-09-22 14:59:46 +0900

    🔨 스킬 체크 난이도 2 문제 풀이

--- src/main/kotlin/programmers/kakao/blind_2021/Solution2.kt
+++ src/main/kotlin/programmers/kakao/blind_2021/Solution2.kt
@@ -9,6 +9,7 @@ import programmers.kakao.blind_2021.network.RetrofitClient
 import programmers.kakao.blind_2021.network.model.*
 import java.util.concurrent.TimeUnit
 import javax.swing.ScrollPaneConstants
+import kotlin.random.Random
 
 val client = RetrofitClient.instance()
 val api = client.create(Api::class.java)
@@ -16,28 +17,46 @@ val api = client.create(Api::class.java)
 var auth_key: String? = null
 var finished = false
 var command: Command? = null
+var temp = 6
+var map = Array(5) { x ->
+    temp -= 2
+    IntArray(5) { y ->
+        x + temp + y * 5
+    }
+}
+
 
 fun main(args: Array<String>) {
-    Observable.concat(
-        api.postStart(1),
+    Observable.concat(api.postStart(1),
         Observable.zip(api.getLocations(), api.getTrucks()) { location, truck ->
+
             Command(
                 mutableListOf<CommandX>().apply {
                     for (i in truck.trucks.indices) {
-                        add(CommandX(listOf(0, 0, 0, 0, 0), truck.trucks[i].id))
+                        add(
+                            CommandX(
+                                listOf(0,0,0,0,0), truck.trucks[i].id
+                            )
+                        )
                     }
+
                 }
             )
-        }.concatMap {
-            api.simulate(it)
-        }.repeatUntil { finished }, api.getScore()
-    ).observeOn(Schedulers.io())
+        }.concatMap { api.simulate(it) }
+            .repeatUntil { finished }, api.getScore()
+    )
+        .observeOn(Schedulers.io())
         .subscribe {
             if (it is Start)
                 auth_key = it.auth_key
 
-            if (it is Simulate)
+            if(it is Simulate){
+                if(it.time == 360)
+                    println(it)
+
                 finished = it.status == "finished"
+            }
+
             if(it is Score)
                 println(it)
         }
